6a873f5041e38364c72e0138eccbd75050be9fc6
sylvia31096 <sylviachach@gmail.com>
2021-03-12 16:42:29 +0300

    first commit

---
+++ src/test/java/com/example/financialInclusion/FinancialInclusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.financialInclusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialInclusionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
